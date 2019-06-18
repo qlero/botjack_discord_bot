11e2e2ceb6030c1c0a7ce806ff7d6fb0a327e5ee
LMquentinLR <quentin.leroux@edhec.com>
2019-06-18 00:17:43 -0400

    help function enhanced v0.1

--- README.md
+++ README.md
@@ -2,10 +2,12 @@ Desc: Simple discord bot implementation
 
 Available functionalities:
 1. Help: BotJack provides the available functionalities when prompted with !help
-2. Dice: BotJack rolls dice for you (see description below)
+2. Dice: BotJack rolls dice for you
 3. Avatar: Botjack cites the source of her avatar picture when prompted with !avatar
 
-Dice functionality: By typing !r 'x'd'y'
-1. where 'x' is a positive integer
-2. where 'y' is a positive integer
-BotJack will throw 'x' 'y'-sided dice and report the sum amount
+Help functionalities: 
+1. generic help can be accessed through the command !help
+2. users can request help on a specific command by typing ?help followed by the problematic command (the ! is optional)
+
+Dice functionalities: By typing !roll followed by a dice amount such as "1d20" or "4d6"
+BotJack will throw the requested x amount of y-sided dice and report the sum amount
--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-#token: TOKEN_API
-
 import discord
 import random
 
-token = "TOKEN_API"
+#################/!\#################
+#to remove before uploading to github
+#################/!\#################
+token = "API TOKEN"
+#################/!\#################
+#################/!\#################
+
 client = discord.Client()
 
-functions = [
-	"**!help** - help, dah!\n",
-	"**!avatar** - so you know who redid my face.\n",
-	"**!r** - let me help you roll dice by writing '***!r 1d6***' for instance!\n"
-]
+functions = {
+	"!help" : "Help, dah!",
+	"!avatar" : "So you may know who rebuilt my face.",
+	"!roll" : "Let me roll dice for you by writing this command followed by '***!r 1d6***' for instance. Go Fish!"
+}
 
 @client.event
 async def on_ready():
@@ -25,14 +29,28 @@ async def on_message(message):
 	#if block to stop the bot from responding to itself
 	if message.author == client.user:
 		return
+	
 	#if block to trigger the !help functionality
 	if message.content.startswith("!help"):
-		msg_1 = "Hello {0.author.mention}.\n".format(message)
-		msg_2 = f"I currently have {len(functions)} commands.\n"
+		msg_1 = "**Available Commands**\nHello {0.author.mention}. ".format(message)
+		msg_2 = f"I currently have {len(functions)} commands."
 		msg_3 = ""
-		for item in functions:
-			msg_3 += item
-		await message.channel.send(msg_1+msg_2+msg_3)
+		msg_4 = "\nFor help with a particular command, use ``?help`` followed by the command name."
+		for index, key in enumerate(functions.keys()):
+			msg_3 += key + ", " if index + 1 < len(functions) else key
+		await message.channel.send(msg_1+msg_2+"```"+msg_3+"```"+msg_4)
+	
+	#if block to trigger the ?help functionality
+	if message.content.startswith("?help "):
+		msg = "Beep, boop! I'm not a smart pony!"
+		split_msg = str(message.content).split(" ")
+		if (split_msg[1] in functions.keys()) or ("!" + split_msg[1] in functions.keys()):
+			if split_msg[1] in functions.keys():
+				msg = functions[split_msg[1]]
+			else:
+				msg = functions["!" + split_msg[1]]
+		await message.channel.send(msg)
+	
 	#if block to trigger the !avatar functionality
 	if message.content.startswith("!avatar"):
 		msg_1 = "Hello {0.author.mention}.\n".format(message)
@@ -40,16 +58,17 @@ async def on_message(message):
 		"Please check their reddit:\n" + \
 		"https://www.reddit.com/user/ScarfyAce/"
 		await message.channel.send(msg_1 + msg_2)
+	
 	# if block to trigger the !r 'i.e dice' functionality
-	if message.content.startswith("!r "):
-		msg = "Beep, boop! I am a stupid pony!"
+	if message.content.startswith("!roll "):
+		msg = "Beep, boop! I'm not a smart pony!"
 		split_msg = str(message.content).split(" ")
 		print("{0.author.mention} requested a die throw.".format(message))
 		for bit in split_msg:
 			split = bit.split("d")
 			if len(split) == 2:
 				if split[0].isdigit() and split[1].isdigit():
-					if msg == "Beep, boop! I am a stupid pony!":
+					if msg == "Beep, boop! I'm not a smart pony!":
 						msg = "Your results, {0.author.mention}!\n".format(message)
 						msg += f"{random.randrange(0, int(split[0]) * int(split[1]) + 1)}\n"
 					else:
