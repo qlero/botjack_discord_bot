40fe28dd62feede1bb7cc968c6d9a4c0c85f13fd
LMquentinLR <quentin.leroux@edhec.com>
2019-09-03 07:52:09 -0400

    test ML v0.1

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import os
 import discord
+import asyncio
 import commands
+import logging
+from keras.models import load_model
+from keras.preprocessing import image
+import cv2
+import numpy as np
 
 #################/!\#################
 #to remove before uploading to github
 #################/!\#################
-token = "API_TOKEN"
+token = "PLACE TOKEN HERE"
 #################/!\#################
 #################/!\#################
 
 client = discord.Client()
+
+dirname, filename = os.path.split(os.path.abspath(__file__))
+model = load_model(dirname + "\\model.h5")
+model.compile(loss = "binary_crossentropy",
+			optimizer = "rmsprop",
+			metrics = ["accuracy"])
+			
+def load_image(img_path):
+	img = cv2.imread(img_path)
+	img = cv2.resize(img, (224,224))
+	img = np.reshape(img, [1, 224, 224, 3])
+	return img
+
+pic_ext = [".jpg", ".png", ".jpeg"]
 
 #List all the available commands in the dictionary below
 functions = {
@@ -43,13 +64,24 @@ async def on_message(message):
 	# if block to trigger the !r 'i.e dice' functionality
 	if message.content.startswith("!roll "):
 		await message.channel.send(commands.roll(message))
-
+	
+	try:
+		print(type(message.attachments), message.attachments, list(message.attachments))
+		attachment = message.attachments[0]
+		print(attachment)
+		for ext in pic_ext:
+			if attachment.endswith(ext):
+				img = load_image(attachment)
+				classes = model.predict_classes(img)
+				if classes[0][0] == 0:
+					await message.channel.send("This image is safe.")
+				else:
+					await message.channel.send("This image is unsafe.")
+	except IndexError:
+		pass
+	
 @client.event
 async def on_error(event, *args, **kwargs):
-	#Gets the message object
-	message = args[0]
-	#logs the error
-	logging.warning(traceback.format_exc())
 	#send the message to the channel
 	await message.channel.send("I am dying--again--beep, boop!")
 
